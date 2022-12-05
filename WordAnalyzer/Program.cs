044c00b6361e0c7c34fafb4bb402771d50017c6e
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-12-05 22:43:55 +0400

    Assignment done without TOP100

--- WordAnalyzer/Program.cs
+++ WordAnalyzer/Program.cs
@@ -1,42 +1,79 @@
 ﻿
+using System.Diagnostics;
+
 namespace WordsAnalyzer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            Thread counter = new(Counter);
+
             Thread thread1 = new(LongestWord);
             Thread thread2 = new(ShortestWord);
+            //Thread thread3 = new(Top100Words); 
 
+            counter.Start();
             thread1.Start();
             thread2.Start();
-
-            //Task.Run(() => {
-            //    ShortestWord();
-            //});
-
-            //Task.Run(() => {
-            //    LongestWord();
-            //});
+           // thread3.Start();
 
 
-            Console.ReadKey();
+
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed.TotalSeconds);
+            return;
+
+            
         }
 
 
-        public static void TopWords()
-        {
-         
+        //finds all words which contains vowel
 
+        public static void Top100Words()
+        {
+
+            string path = @"C:\Users\varda\Desktop\Practice4\practice 4\WordsAnalyzer\File\words_alpha.txt";
+            string result = File.ReadAllText(path);
+           
+
+            int vowelCount = 0;
+            int consCount = 0;
+            var len = result.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (result[i] == 'a' || result[i] == 'e' || result[i] == 'i' || result[i] == 'o' || result[i] == 'u' ||
+                    result[i] == 'A' || result[i] == 'E' || result[i] == 'I' || result[i] == 'O' || result[i] == 'U')
+                {
+                    vowelCount++;
+
+                }
+                else
+                {
+                    consCount++;
+                }
+            }
+
+
+            var vowel = vowelCount.ToString(result);
+            Console.WriteLine(vowel);
         }
 
 
         public static void LongestWord()
         {
+            Thread.Sleep(500);
+            Console.WriteLine("Longest Word is found!");
+            Thread.Sleep(1500);
+
+
             string path = @"C:\Users\varda\Desktop\Practice4\practice 4\WordsAnalyzer\File\words_alpha.txt";
             string result = File.ReadAllText(path);
-            var words = new List<string>(result.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries));
-            Console.WriteLine($"Number Of Words : {words.Count}");
+            var words = new List<string>(result.Split(new string[] { }, StringSplitOptions.RemoveEmptyEntries));
+          //  Console.WriteLine($"Number Of Words : {words.Count}");
             if (words.Count > 0)
             {
 
@@ -56,18 +93,21 @@ namespace WordsAnalyzer
 
             }
 
-            Thread.Sleep(1500);
-            Console.WriteLine("Longest Word is found!");
+            
 
         }
 
         public static void ShortestWord()
         {
-            // Thread.Sleep(2000);
+            Thread.Sleep(500);
+            Console.WriteLine("Shortest Word is found!");
+            Thread.Sleep(1500);
+
+
             string path = @"C:\Users\varda\Desktop\CredoBankAssignments\WordAnalyzer\File\words_alpha.txt";
             string result = File.ReadAllText(path);
-            var words = new List<string>(result.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries));
-            Console.WriteLine($"Number Of Words : {words.Count}");
+            var words = new List<string>(result.Split(new string[] { }, StringSplitOptions.RemoveEmptyEntries));
+          //  Console.WriteLine($"Number Of Words : {words.Count}");
             if (words.Count > 0)
             {
 
@@ -85,8 +125,15 @@ namespace WordsAnalyzer
 
             
             }
-            Thread.Sleep(1000);
-            Console.WriteLine("Shortest Word is found!");
+            
+        }
+
+        public static void Counter()
+        {
+            string path = @"C:\Users\varda\Desktop\CredoBankAssignments\WordAnalyzer\File\words_alpha.txt";
+            string result = File.ReadAllText(path);
+            var words = new List<string>(result.Split(new string[] { }, StringSplitOptions.RemoveEmptyEntries));
+            Console.WriteLine($"Number Of Words : {words.Count}");
         }
 
     }
