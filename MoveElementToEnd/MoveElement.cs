0228f7712d1dd71efe0332aebaa9e9e49b4cfa54
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-12-19 19:53:39 +0400

    Add Move Element To End

---
+++ MoveElementToEnd/MoveElement.cs
@@ -0,0 +1,34 @@
+﻿namespace MoveElementToEnd
+{
+    public class MoveElement
+    {
+
+        public static void MoveElementsToEnd(List<int> array, int ToMove)
+        {
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] == 2)
+                {
+                    ToMove++;
+
+                    array.RemoveAt(i);
+                    i--;
+                }
+            }
+            for (int i = 0; i < ToMove; i++)
+            {
+
+                array.Add(2);
+            }
+
+            for (int i = 0; i < array.Count; i++)
+            {
+
+                Console.Write(string.Join(", ", array[i]) + " ");
+            }
+        }
+
+    }
+    
+}
---
+++ MoveElementToEnd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace MoveElementToEnd
+{
+
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                var array = new List<int> { 2, 1, 2, 2, 2, 3, 4, 2 };
+                var toMove = 2;
+
+                MoveElement.MoveElementsToEnd(array, toMove);
+               
+            }
+        }
+    
+}
