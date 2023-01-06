0207064c4e95ca5740d19b6a3c5eb9531c9db3ee
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-01-06 11:25:16 +0400

    Assignments

--- WarehouseManagementSystem/Program.cs
+++ WarehouseManagementSystem/Program.cs
@@ -1,2 +1,106 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using WarehouseManagementSystem;
+
+namespace WarehouseSystem
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var warehouse = new WareHouseSys();
+            var exit = false;
+            while (!exit)
+            {
+                MenuOptions();
+            }
+
+            void MenuOptions()
+            {
+                Console.WriteLine("1 - Register");
+                Console.WriteLine("2 - Update");
+                Console.WriteLine("3 - Delete");
+                Console.WriteLine("4 - Show all");
+                Console.WriteLine("5 - Exit");
+
+                Console.Write("Please enter the index: ");
+                var options = int.Parse(Console.ReadLine());
+
+                switch (options)
+                {
+                    case 1:
+                        Register();
+                        break;
+                    case 2:
+                        Update();
+                        break;
+                    case 3:
+                        Delete();
+                        break;
+                    case 4:
+                        ShowAll();
+                        break;
+                    case 5:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Choose numbers between 1 to 5");
+                        break;
+                }
+
+            }
+
+
+            void Register()
+            {
+                Console.Write("Name: ");
+                var productName = Console.ReadLine();
+
+                Console.Write("Category: ");
+                var category = Console.ReadLine();
+
+                Console.Write("Price: ");
+                var price = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Quantity: ");
+                var quantity = double.Parse(Console.ReadLine());
+
+                warehouse.ProductAdd(productName, category, price, quantity);
+            }
+
+            void Update()
+            {
+                Console.Write("Name: ");
+                var productName = Console.ReadLine();
+
+                Console.Write("Category: ");
+                var category = Console.ReadLine();
+
+                Console.Write("Price: ");
+                var price = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Quantity: ");
+                var quantity = double.Parse(Console.ReadLine());
+
+                warehouse.UpdateProduct(productName, price, quantity);
+            }
+
+            void Delete()
+            {
+                Console.Write("Name: ");
+                var productName = Console.ReadLine();
+                warehouse.ProductRemove(productName);
+            }
+
+            void ShowAll()
+            {
+                warehouse.ShowAllProduct();
+
+            }
+
+
+
+        }
+    }
+}
--- WarehouseManagementSystem/WareHouseSys.cs
+++ WarehouseManagementSystem/WareHouseSys.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace WarehouseManagementSystem
 {
-    internal class WareHouse
+    public class WareHouseSys
     {
+        public List<Product> Products { get; set; }
+        public WareHouseSys()
+        {
+            Products = new List<Product>();
+        }
+
+        public void ProductAdd(string? name, string? category, decimal price, double quantity)
+        {
+            var product = new Product(name, category, price, quantity);
+            Products.Add(product);
+
+        }
+
+        public void UpdateProduct(string? name, decimal price, double quantity)
+        {
+            var product = FindProductByName(name);
+            if (product == null)
+            {
+                return;
+            }
+            product.Price = price;
+            product.Quantity = quantity;
+
+        }
+
+
+        public void ProductRemove(string? name)
+        {
+            var product = FindProductByName(name);
+            if (product == null)
+            {
+                return;
+            }
+            Products.Remove(product);
+        }
+
+        public void ShowAllProduct()
+        {
+            foreach (var product in Products)
+            {
+                Console.WriteLine(product.ToString());
+            }
+        }
+
+
+
+        private Product? FindProductByName(string? name)
+        {
+            var product = Products.FirstOrDefault(k => k.Name == name);
+            return product;
+        }
+
+
     }
 }
