038f688149c615a73a1d28f60879a737b8ae266d
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-01-16 15:53:24 +0400

    First Project

--- ToDoAPP.Api/Db/AppDbContextFactory.cs
+++ ToDoAPP.Api/Db/AppDbContextFactory.cs
@@ -3,15 +3,16 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace ToDoApp.Api.Db
 {
-    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
-    {
-        public AppDbContext CreateDbContext(string[] args)
-        {
-            Console.WriteLine(args[0]);
-            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer(args[0]);
+    //public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    //{
+    //    //public AppDbContext CreateDbContext(string[] args)
+    //    //{
+    //    //    Console.WriteLine(args[0]);
+    //    //    var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+    //    //    optionsBuilder.UseSqlServer(args[0]);
 
-            return new AppDbContext(optionsBuilder.Options);
-        }
-    }
+    //    //    return new AppDbContext(optionsBuilder.Options);
+    //    //}
+        
+    //}
 }
--- ToDoAPP.Api/Program.cs
+++ ToDoAPP.Api/Program.cs
@@ -1,11 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Api.Auth;
+using ToDoApp.Api.Db;
+using ToDoApp.Api.Db.Models;
+using ToDoAPP.Api.Auth;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<IAuthService, AuthService>();
+AuthConfigurator.Configure(builder);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//ent
+var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+optionsBuilder.UseSqlServer("Server=localhost;Database=todoapp_db;User Id=sa;Password=pass123;");
+
+var db = new AppDbContext(optionsBuilder.Options);
+
+db.Todos.Add(new TodoEntity
+{
+    Id = 1,
+    UserId = 1,
+    StatusId = 1,
+    Name = "person 1",
+    Description = "project_1",
+    Deadline = DateTime.Now
+
+});
+
+//ent
+
+
 
 var app = builder.Build();
 
