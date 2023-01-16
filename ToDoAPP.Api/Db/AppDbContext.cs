d57318d8803eeee7934ab81a7c5b0252dde866be
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-01-16 22:28:40 +0400

    First Project

--- ToDoAPP.Api/Db/AppDbContext.cs
+++ ToDoAPP.Api/Db/AppDbContext.cs
@@ -9,12 +9,29 @@ namespace ToDoApp.Api.Db
         {
 
         }
-
+        public DbSet<TodoEntity> Todos { get; set; }
         public AppDbContext(DbContextOptions options) : base(options)
         {
 
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=todoapp_db;User Id=sa;Password=pass123;");
+
+            var db = new AppDbContext(optionsBuilder.Options);
+
+            db.Todos.Add(new TodoEntity
+            {
+                Id = 1,
+                UserId = 1,
+                StatusId = 1,
+                Name = "person 1",
+                Description = "project_1",
+                Deadline = DateTime.Now
+
+            });
+
+            db.SaveChanges();
         }
 
-        public DbSet<TodoEntity> Todos { get; set; }
+       
     }
 }
