9eaaef3bce4622c4ca2c9ae7679cb76a89be4c99
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-01-17 23:18:36 +0400

    First Project

--- ToDoAPP.Api/Program.cs
+++ ToDoAPP.Api/Program.cs
@@ -17,22 +17,22 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-//ent
-var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-optionsBuilder.UseSqlServer("Server=localhost;Database=todoapp_db;User Id=sa;Password=pass123;");
+////ent
+//var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+//optionsBuilder.UseSqlServer("Server=localhost;Database=todoapp_db;User Id=sa;Password=pass123;");
 
-var db = new AppDbContext(optionsBuilder.Options);
+//var db = new AppDbContext(optionsBuilder.Options);
 
-db.Todos.Add(new TodoEntity
-{
-    Id = 1,
-    UserId = 1,
-    StatusId = 1,
-    Name = "person 1",
-    Description = "project_1",
-    Deadline = DateTime.Now
+//db.Todos.Add(new TodoEntity
+//{
+//    Id = 1,
+//    UserId = 1,
+//    StatusId = 1,
+//    Name = "person 1",
+//    Description = "project_1",
+//    Deadline = DateTime.Now
 
-});
+//});
 
 //ent
 
