f5ed4104f0de16f017e8b10899134e21560cd1da
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-02-06 17:24:18 +0400

    Add Db

---
+++ BonusManagementSystem_Api/Db/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BonusManagementSystem_Api.Db
+{
+    public class AppDbContext : DbContext
+    {
+       
+
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ BonusManagementSystem_Api/Db/Entity/EmpoyeeEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace BonusManagementSystem_Api.Db.Entity
+{
+    public class EmpoyeeEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PrivateNumber { get; set; }
+        public double Salary { get; set; }
+        public string Recomedator { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
