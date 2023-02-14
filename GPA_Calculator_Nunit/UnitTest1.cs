d5393d2b9377fc062b3004ebc827bbb522a97959
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-02-15 01:09:30 +0400

    delete Nunit

---
+++ GPA_Calculator_Nunit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GPA_Calculator_Nunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
